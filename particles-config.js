45b8d084e08f370b96d8ba7917c79291533a54ad
Dimply <dimply2004@gmail.com>
2025-04-29 09:16:12 +0530

    hi

---
+++ particles-config.js
@@ -0,0 +1,15 @@
+particlesJS('particles-js', {
+    particles: {
+      number: { value: 80 },
+      size: { value: 4 },
+      move: { speed: 2 },
+      line_linked: { enable: true },
+      color: { value: "#ffffff" }
+    },
+    interactivity: {
+      events: {
+        onhover: { enable: true, mode: "repulse" }
+      }
+    }
+  });
+  
---
+++ script.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x6ca0dc,
+    shininess: 50.00,
+    waveHeight: 20.00,
+    waveSpeed: 0.7,
+    zoom: 0.85
+  })
+  
